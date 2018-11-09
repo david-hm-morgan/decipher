dba001a3c5d828927013f0ed26d6f72b59094db9
David HM Morgan <david_hm_morgan@hotmail.com>
2018-11-09 19:37:27 +0000

    add dual purpose decoding

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require('express')
 const app = express()
 const port = 3000
 const index = require('./index')
+const currentPuzzle = require('./nov8Translation')
 
-var key = index.getKey();
-var encryptedText = index.getEncryptedText();
 var decoderFunction = index.getDecoder();
 
-var decoderOutptut = decoderFunction(encryptedText, key);
+var keyA = currentPuzzle.getKeyA();
+var keyB = currentPuzzle.getKeyB();
+var encryptedTextA = currentPuzzle.getEncryptedTextA();
+var encryptedTextB = currentPuzzle.getEncryptedTextB();
+
+var decoderOutputA = decoderFunction(encryptedTextA, keyA);
+var decoderOutputB = decoderFunction(encryptedTextB, keyB);
 
 var htmlOutput = "<meta http-equiv=\"refresh\" content=\"5\">";
 htmlOutput += "<table><th>Encrypted Text</th><th>Decrypted Text</th>";
-htmlOutput += `<tr><td><code>${encryptedText}</code></td>`
-htmlOutput += `<td><code>${decoderOutptut}</code></td></tr>`
+htmlOutput += `<tr><td><code>${encryptedTextA}</code></td>`
+htmlOutput += `<td><code>${decoderOutputA}</code></td></tr>`
+htmlOutput += "</table>";
+
+
+htmlOutput += "<table><th>Encrypted Text</th><th>Decrypted Text</th>";
+htmlOutput += `<tr><td><code>${encryptedTextB}</code></td>`
+htmlOutput += `<td><code>${decoderOutputB}</code></td></tr>`
 htmlOutput += "</table>";
 
 app.get('/', (req, res) => res.send(`${htmlOutput}`))
