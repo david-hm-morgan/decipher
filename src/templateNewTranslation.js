8857a04ba0789a8619c2b92aded30b65f43c10c2
David HM Morgan <david_hm_morgan@hotmail.com>
2018-11-09 19:04:37 +0000

    add template for new codes

---
+++ src/templateNewTranslation.js
@@ -0,0 +1,103 @@
+function eighthNovemberTranslationA() {
+    let translation = {};
+    
+    translation.K = "_";
+    translation.R = "_";
+    translation.E = "_";
+    translation.A = "_";
+    translation.L = "_";
+    translation.P = "_";
+    translation.O = "_";
+
+    translation.I = "_";
+
+    translation.T = "_";
+    translation.U = "_";
+    translation.V = "_";
+    translation.W = "_";
+    translation.X = "_";
+    translation.Y = "_";
+    translation.Z = "_";
+    translation.B = "_";
+    translation.C = "_"; 
+    translation.D = "_";
+    translation.F = "_";
+    translation.G = "_";
+    translation.H = "_";
+    translation.J = "_";
+    translation.M = "_";
+
+    translation.N = "_";
+    translation.Q = "_";
+    translation.S = "_";
+
+
+
+    // punctuation
+    translation["'"] = "'";
+    translation[","] = ",";
+    translation["."] = ".\n";
+    translation["!"] = "!";
+    translation["?"] = "?";
+    translation["’"] = "’";
+    translation[")"] = ")";
+    translation["("] = "(";
+    translation["“"] = "“";
+    translation["”"] = "”";
+    translation["-"] = "-";
+    translation[":"] = ":";
+    
+    return translation;
+}
+
+function eighthNovemberTranslationB() {
+    let translation = {};
+    
+    translation.K = "_";
+    translation.R = "_";
+    translation.E = "_";
+    translation.A = "_";
+    translation.L = "_";
+    translation.P = "_";
+    translation.O = "_";
+
+    translation.I = "_";
+
+    translation.T = "_";
+    translation.U = "_";
+    translation.V = "_";
+    translation.W = "_";
+    translation.X = "_";
+    translation.Y = "_";
+    translation.Z = "_";
+    translation.B = "_";
+    translation.C = "_"; 
+    translation.D = "_";
+    translation.F = "_";
+    translation.G = "_";
+    translation.H = "_";
+    translation.J = "V";
+    translation.M = "_";
+
+    translation.N = "_";
+    translation.Q = "_";
+    translation.S = "_";
+
+
+
+    // punctuation
+    translation["'"] = "'";
+    translation[","] = ",";
+    translation["."] = ".\n";
+    translation["!"] = "!";
+    translation["?"] = "?";
+    translation["’"] = "’";
+    translation[")"] = ")";
+    translation["("] = "(";
+    translation["“"] = "“";
+    translation["”"] = "”";
+    translation["-"] = "-";
+    translation[":"] = ":";
+    
+    return translation;
+}
