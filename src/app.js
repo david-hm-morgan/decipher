21b4efd8328b20a37843a1afcd486dbf396d6fb8
David HM Morgan <david_hm_morgan@hotmail.com>
2018-11-10 09:16:46 +0000

    display frequency results

--- src/app.js
+++ src/app.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const index = require('./index')
-const currentPuzzle = require('./nov8Translation')
+const express = require('express');
+const app = express();
+const port = 3000;
+const index = require('./index');
+const currentPuzzle = require('./nov8Translation');
+const singleFreq = require('./freqAnalysis');
 
 var decoderFunction = index.getDecoder();
 
 var keyA = currentPuzzle.getKeyA();
 var keyB = currentPuzzle.getKeyB();
-var encryptedTextA = currentPuzzle.getEncryptedTextA();
-var encryptedTextB = currentPuzzle.getEncryptedTextB();
+let encryptedTextA = currentPuzzle.getEncryptedTextA();
+let encryptedTextB = currentPuzzle.getEncryptedTextB();
 
 var decoderOutputA = decoderFunction(encryptedTextA, keyA);
 var decoderOutputB = decoderFunction(encryptedTextB, keyB);
+
+var frequency = singleFreq.sorter();
 
 var htmlOutput = "<meta http-equiv=\"refresh\" content=\"5\">";
 htmlOutput += "<table><th>Encrypted Text</th><th>Decrypted Text</th>";
 htmlOutput += `<tr><td><code>${encryptedTextA}</code></td>`
 htmlOutput += `<td><code>${decoderOutputA}</code></td></tr>`
+htmlOutput += `<td>${frequency}</td>`;
+htmlOutput += `<td></td>`;
 htmlOutput += "</table>";
 
 
