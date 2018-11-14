ee4401df015b27ef732046af2b7538a79074a7dd
David HM Morgan <david_hm_morgan@hotmail.com>
2018-11-14 18:45:25 +0000

    add placeholders for new puzzles this week

---
+++ src/nov15TranslationA.js
@@ -0,0 +1,62 @@
+let eighthNovemberStringToTranslateA = `(Replace this text with your message to decipher)`;
+
+function eighthNovemberTranslationA() {
+    let translation = {};
+    
+    translation.K = "_";
+    translation.R = "_";
+    translation.E = "_";
+    translation.A = "_";
+    translation.L = "_";
+    translation.P = "_";
+    translation.O = "_";
+
+    translation.I = "_";
+
+    translation.T = "_";
+    translation.U = "_";
+    translation.V = "_";
+    translation.W = "_";
+    translation.X = "_";
+    translation.Y = "_";
+    translation.Z = "_";
+    translation.B = "_";
+    translation.C = "_"; 
+    translation.D = "_";
+    translation.F = "_";
+    translation.G = "_";
+    translation.H = "_";
+    translation.J = "_";
+    translation.M = "_";
+
+    translation.N = "_";
+    translation.Q = "_";
+    translation.S = "_";
+
+
+
+    // punctuation
+    translation["'"] = "'";
+    translation[","] = ",";
+    translation["."] = ".\n";
+    translation["!"] = "!";
+    translation["?"] = "?";
+    translation["’"] = "’";
+    translation[")"] = ")";
+    translation["("] = "(";
+    translation["“"] = "“";
+    translation["”"] = "”";
+    translation["-"] = "-";
+    translation[":"] = ":";
+    
+    return translation;
+}
+
+module.exports = {
+    getEncryptedTextA: function() {
+        return eighthNovemberStringToTranslateA;
+    },
+    getKeyA: function() {
+        return eighthNovemberTranslationA();
+    }
+}
