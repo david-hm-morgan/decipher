2f7473dac19eba64650b169df58b2b6321e9276c
David HM Morgan <david_hm_morgan@hotmail.com>
2018-11-15 18:24:36 +0000

    cracked the 15th A code

--- src/nov15TranslationA.js
+++ src/nov15TranslationA.js
@@ -1,37 +1,39 @@
-let eighthNovemberStringToTranslateA = `(AAAAAAAAAAAAAReplace this text with part A's text to decipher)`;
+let eighthNovemberStringToTranslateA = `WJONH TNGYN CTTKH JQWCJ VIHVN TDONG UVWMI AKJNJ ZINHJ WVNTL DKCTW JJVIH VIZMI HBNGU VITDL LWCJD JVISZ DDBNC TBDHJ DLJVI BOIGI IBEJQ JVDKS VJVIG IONHJ VIDUU NHWDC NZADP DLENE IGHBN GYITH NOVWU VVNTU ZINGZ QAIIC LDGSD JJICW CJVIB DMIJV IQODK ZTAIW CJIGI HJWCS JDGIN TZNJI GAKJC DJVWC SWCJV IBZDD YITWB EDGJN CJICD KSVJD VNMII PUWJI TBQUD UDCHE WGNJD GOVDI MIGJV NJBWS VJAIW JODKZ TVNMI AIICI NHQJD SIJTW HUDKG NSITA KJWCI ITITJ DGIUD MIGJV IZWTN GHDWU NGGWI TDCHI NGUVW CSLDG JVIUV WBCIQ WIMIC JKNZZ QLDKC TWJNJ JVIAN UYDLJ VIHJN UYHAK JJVIG IONHC DHWSC DLJVI ZWTNG DGDLN CQTWH JKGAN CUIJD HKSSI HJWJV NTLNZ ZICJV NJLNG HDWSK IHHIT WJBWS VJVNM ISDJU NKSVJ DCNZI TSIVW SVIGK ELDGJ KCNJI ZQJVI UVWBC IQONH LNWGZ QOWTI AKJCD JHDOW TIJVN JWUDK ZTCJA GWTSI WJNCT NZZJV DHIVD KGHDC JVIUZ WBAWC SONZZ ENWTD LLNHW UZWBA ITKEZ DDYWC SLDGJ VIZDH JBNUV WCINH WHKHE IUJIT WJVNT UNKSV JDCDC IDLJV IZITS IHTIH WSCIT JDUNJ UVGNW CHDWB NTIGI NTQJD ZDOIG WJANU YTDOC AKJNH WBDMI TZDDH IAGWU YHJDH JINTQ BQHIZ LENGJ DLJVI WCCIG ONZZU DZZNE HITNC TJDBQ NBNRI BICJW LDKCT BQHIZ LHJNG WCSWC JDNUD CJGDZ GDDBW JONHM IGQXK ZIHMI GCINC TODKZ TCJVN MIZDD YITDK JDLEZ NUIDC JVIAG WTSID LJVIC NKJWZ KHWJB KHJVN MIAII CUKJJ WCSIT SIJIU VCDZD SQDCU INZZA GNHHN CTEDZ WHVIT WCHJG KBICJ HJVIG IONHN BNEDC JVION ZZUDM IGITW CHBNZ ZAKZA HJVNJ WNHHK BITOD KZTVN MIZWJ KEJDH WSCWL QNUJW MWJQN CTNAN CYDLL WCICW CIJII CJVUI CJKGQ JIZIS GNEVB NUVWC IHWCE DZWHV ITONZ CKJKC ZIHHW ONHBW HJNYI CWVNT LDKCT TDKSZ NHAZN UYHUD BBNCT UICJG IKCTI GNZNG SIUWG UKZNG DNYJN AZIWC JVIUI CJGID LJVIG DDBJV IGION HNLWZ WCSHQ HJIBO WJVTG NOIGH UDCJN WCWCS BNEHD LIMIG QCNJW DCNCT JVIJN AZIUD KZTAI JKGCI TZWYI NGIHJ NKGNC JZNRQ HKHNC JVION ZZHOI GIZWC ITOWJ VAGNH HADKC TZITS IGHNC TLDZT IGHBN GYITO WJVOV NJZDD YITZW YIBWH HWDCU DTICN BIHWC NBDCS HJJVI BONHJ VIBDH JNBNR WCSLW CTAZN UYHUD TIADD YWJUD CJNWC ITNCK BAIGD LYIQH NCTAI HJDLN ZZNZW HJDLU WEVIG HNCTO VICNC TVDOJ VIQHV DKZTA IKHIT WJODK ZTAIO DGJVN LDGJK CIDCJ VIAWA ZWDEV WZIAZ NUYBN GYIJA KJWTW TCJLI IZZWY IHIZZ WCSWJ VNGGQ BWSVJ AIWCJ IGIHJ ITWLD CZQLD GVWHJ DGWUN ZGINH DCHAK JWLCD JJVIC JVWHO DKZTL WCTNV DBIWC BQDOC EGWMN JIUDZ ZIUJW DCWJD DYNHI NJAQJ VIJIZ ISGNE VNCTE KJBQL IIJKE DEICW CSJVI UDTIA DDYNJ GNCTD BJDGI NTNHO WJVJV IJVGI IIBEI GDGHD EIGNJ WDCWJ BNQAI CIUIH HNGQJ DICVN CUIDE IGNJW DCNZH IUKGW JQTKG WCSJV IDEIG NJWDC WJHIZ LWCJV NJUNH IOIBD MITLG DBEKG IZQZI JJIGH KAHJW JKJWD CUGQE JDSGN BHJDJ GNCHE DHWJW DCUWE VIGHN CTNJJ VIKGS WCSDL ANGDC EZNQL NWGDJ VIGBD GIUDB EZWUN JITUW EVIGH ZWYIJ VIMWS ICIGI NCTEZ NQLNW GUWEV IGHNC TMNGW NCJHK EDCJV IBWON HUDBE ZIJIZ QWBBI GHITW COVNJ WONHG INTWC SNCTJ VICWJ VWJBI`;
+
+let decoded = `itwasdarkanddustyintheshadowarchivebutatleastihadfoundittheshelvesmarchedoffintothegloomandmostofthemwereemptythoughtherewastheoccasionalboxofpapersmarkedsawhichhadclearlybeenforgotteninthemovetheywouldbeinterestingtoreadlaterbutnothinginthemlookedimportantenoughtohaveexcitedmycoconspiratorwhoeverthatmightbeitwouldhavebeeneasytogetdiscouragedbutineededtorecoverthelidarsoicarriedonsearchingforthechimneyieventuallyfounditatthebackofthestacksbuttherewasnosignofthelidarorofanydisturbancetosuggestithadfallenthatfarsoiguesseditmighthavegotcaughtonaledgehigherupfortunatelythechimneywasfairlywidebutnotsowidethaticouldntbridgeitandallthosehoursontheclimbingwallpaidoffasiclimbeduplookingforthelostmachineasisuspectedithadcaughtononeoftheledgesdesignedtocatchrainsoimadereadytoloweritbackdownbutasimovedloosebrickstosteadymyselfpartoftheinnerwallcollapsedandtomyamazementifoundmyselfstaringintoacontrolroomitwasveryjulesverneandwouldnthavelookedoutofplaceonthebridgeofthenautilusitmusthavebeencuttingedgetechnologyonceallbrassandpolishedinstrumentstherewasamaponthewallcoveredinsmallbulbsthatiassumedwouldhavelituptosignifyactivityandabankoffinenineteenthcenturytelegraphmachinesinpolishedwalnutunlessiwasmistakenihadfounddouglasblackscommandcentreunderalargecircularoaktableinthecentreoftheroomtherewasafilingsystemwithdrawerscontainingmapsofeverynationandthetablecouldbeturnedlikearestaurantlazysusanthewallswerelinedwithbrassboundledgersandfoldersmarkedwithwhatlookedlikemissioncodenamesinamongstthemwasthemostamazingfindblackscodebookitcontainedanumberofkeysandbestofallalistofciphersandwhenandhowtheyshouldbeuseditwouldbeworthafortuneonthebibliophileblackmarketbutididntfeellikesellingitharrymightbeinterestedifonlyforhistoricalreasonsbutifnotthenthiswouldfindahomeinmyownprivatecollectionitookaseatbythetelegraphandputmyfeetupopeningthecodebookatrandomtoreadaswiththethreeemperorsoperationitmaybenecessarytoenhanceoperationalsecurityduringtheoperationitselfinthatcasewemovedfrompurelylettersubstitutioncryptogramstotranspositionciphersandattheurgingofbaronplayfairothermorecomplicatedcipherslikethevigenereandplayfairciphersandvariantsuponthemiwascompletelyimmersedinwhatiwasreadingandthenithitme`;
 
 function eighthNovemberTranslationA() {
     let translation = {};
     
-    translation.K = "_";
-    translation.R = "_";
-    translation.E = "_";
-    translation.A = "_";
-    translation.L = "_";
-    translation.P = "_";
-    translation.O = "_";
+    translation.K = "u";// u? no_ght //
+    translation.R = "z";
+    translation.E = "p";//n?
+    translation.A = "b";//_ut b?
+    translation.L = "f";//_ound f?? /
+    translation.P = "x";
+    translation.O = "w";//w? s?
 
-    translation.I = "_";
+    translation.I = "e"; //according to freq annal and JVI could be the //
 
-    translation.T = "_";
-    translation.U = "_";
-    translation.V = "_";
-    translation.W = "_";
-    translation.X = "_";
-    translation.Y = "_";
-    translation.Z = "_";
-    translation.B = "_";
-    translation.C = "_"; 
-    translation.D = "_";
-    translation.F = "_";
-    translation.G = "_";
-    translation.H = "_";
-    translation.J = "_";
-    translation.M = "_";
+    translation.T = "d";//an_ d? //
+    translation.U = "c";// _aught c? /
+    translation.V = "h"; //JVI could be the //
+    translation.W = "i";// s a e i o u? because comes before t //
+    translation.X = "j";
+    translation.Y = "k";//k?
+    translation.Z = "l";//doubles ll?
+    translation.B = "m";//l? _ong   m? it_ighth__egot
+    translation.C = "n"; //n? //
+    translation.D = "o";//o? ti_n might //
+    translation.F = "z";
+    translation.G = "r";//a_chive r? /
+    translation.H = "s";//a??
+    translation.J = "t"; //t? //
+    translation.M = "v";// v? h__e /
 
-    translation.N = "_";
-    translation.Q = "_";
-    translation.S = "_";
+    translation.N = "a";//w? o? a? h__e /
+    translation.Q = "y";// chimne_ y?
+    translation.S = "g";//k? g? nothin_ might correct
 
 
 
